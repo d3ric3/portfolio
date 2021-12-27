446856cae9985fb0edeacdd1931025ff2e9aa518
d3ric3 <d3ric3@gmail.com>
2021-12-28 00:34:04 +0800

    added hyperlink to listings

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,25 +1,29 @@
 import React from "react";
+// manual get preview data
+// https://link-preview-generator.herokuapp.com/
 
 const Portfolio = ({ data }) => {
   if (data) {
     var listings = data.listings.map(function (listings) {
       return (
-        <div class="preview-data columns portfolio-item">
-          <div class="item-wrap">
-            <div class="img-container">
-              <img
-                class="img"
-                src={listings.image_url}
-                alt="active listing"
-              ></img>
-            </div>
-            <div class="text-container">
-              <span class="domain">{listings.domain}</span>
-              <span class="header">{listings.header}</span>
-              <span class="description">{listings.description}</span>
+        <a href={listings.url}>
+          <div class="preview-data columns portfolio-item">
+            <div class="item-wrap">
+              <div class="img-container">
+                <img
+                  class="img"
+                  src={listings.image_url}
+                  alt="active listing"
+                ></img>
+              </div>
+              <div class="text-container">
+                <span class="domain">{listings.domain}</span>
+                <span class="header">{listings.header}</span>
+                <span class="description">{listings.description}</span>
+              </div>
             </div>
           </div>
-        </div>
+        </a>
       );
     });
   }
